cd6f67d40fc9bbb1aa545dcb9a44834b11509fe2
Gaurav SinghaRoy <neogauravsvnit@gmail.com>
2015-12-05 15:32:09 +0100

    added methods to build apps inside the GenreTopListFactory

--- app/factories/genre_top_list_factory.rb
+++ app/factories/genre_top_list_factory.rb
@@ -10,6 +10,9 @@ class GenreTopListFactory
 
   def build
     build_genre_top_list_basic_attrs
+    build_top_paid_apps
+    build_top_free_apps
+    build_top_grossing_apps
     genre_top_list
   end
 
@@ -19,6 +22,22 @@ class GenreTopListFactory
     self.genre_top_list.id    = response_body['genre']['id']
     self.genre_top_list.name  = response_body['genre']['name']
     self.genre_top_list.url   = response_body['genre']['url']
+  end
+
+  def build_top_paid_apps
+    self.genre_top_list.top_paid_apps = build_app_list(response_body['topCharts'][0]['adamIds'])
+  end
+
+  def build_top_free_apps
+    self.genre_top_list.top_free_apps = build_app_list(response_body['topCharts'][1]['adamIds'])
+  end
+
+  def build_top_grossing_apps
+    self.genre_top_list.top_grossing_apps = build_app_list(response_body['topCharts'][2]['adamIds'])
+  end
+
+  def build_app_list(app_ids)
+    AppListFactory.new(app_ids).build
   end
 
 end
