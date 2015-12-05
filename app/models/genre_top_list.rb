17cd7c0dd8a0769ae71b5e5e692bf514ea17b753
Gaurav SinghaRoy <neogauravsvnit@gmail.com>
2015-12-05 15:15:30 +0100

    generating lists for top paid, free and grossing apps in the factory

--- app/models/genre_top_list.rb
+++ app/models/genre_top_list.rb
@@ -1,7 +1,16 @@
 # This class contains the list of apps in a particular genre
 # This needs to be generated from its factory class GenreTopListFactory
 class GenreTopList
-  attr_accessor :id, :name, :url
+
+  attr_accessor :id, :name,
+    :url, :top_paid_apps,
+    :top_free_apps, :top_grossing_apps
+
+  def initialize
+    @top_paid_apps = []
+    @top_free_apps = []
+    @top_grossing_apps = []
+  end
 
   def sync_app_information!
     # This will sync app information
